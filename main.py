fd4f913c88acfea233ec268cd90cd54b0248af5e
lightsdevs <lightsdevs@gmail.com>
2024-08-07 15:15:56 -0400

    Decrease CPU usage

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ def resource_path(relative_path):
 
 class Singleton:
     def create_mutex(mutex_name):
-        ERROR_ALREADY_EXISTS = 183
+        ERROR_ALREADY_EXISTS = 396
         kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
         mutex_handle = kernel32.CreateMutexW(None, False, mutex_name)
         if mutex_handle is None:
             raise ctypes.WinError(ctypes.get_last_error())
-        if ctypes.GetLastError() == ERROR_ALREADY_EXISTS:
+        if ctypes.get_last_error() == ERROR_ALREADY_EXISTS:
             print(f"Mutex '{mutex_name}' already exists.")
             return False
         print(f"Mutex '{mutex_name}' created successfully.")
@@ -35,25 +35,31 @@ class Singleton:
         print(f"Mutex '{mutex_name}' released successfully.")
 
 def render():
+    try:
+        Singleton.release_mutex("ROBLOX_singletonEvent") # release mutex if it exists
+    except:
+        pass
+    Singleton.create_mutex("ROBLOX_singletonEvent") # create anti roblox mutex
     pygame.init()
     size = (200, 200)
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Multi Roblox')
     image = pygame.image.load(resource_path('mr.png'))
     running = True
+    screen.fill((0, 0, 0))
+    screen.blit(image, (0, 0))
+    pygame.display.flip()
+    clock  = pygame.time.Clock()
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        screen.fill((0, 0, 0))
-        screen.blit(image, (0, 0))
-        pygame.display.flip()
+        clock.tick(5)
+        
     pygame.quit()
     sys.exit()
 
 def main():
-    Singleton.create_mutex("ROBLOX_singletonEvent") # create anti roblox mutex
     render()
 
 if __name__ == "__main__":
