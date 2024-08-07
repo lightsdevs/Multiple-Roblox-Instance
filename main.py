ee355860175b62848c998fda554d4f5cb6609152
lightsdevs <lightsdevs@gmail.com>
2024-08-06 21:18:43 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import pygame
+import sys
+import ctypes
+
+class Singleton:
+    def create_mutex(mutex_name):
+        ERROR_ALREADY_EXISTS = 183
+        kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+        mutex_handle = kernel32.CreateMutexW(None, False, mutex_name)
+        if mutex_handle is None:
+            raise ctypes.WinError(ctypes.get_last_error())
+        if ctypes.GetLastError() == ERROR_ALREADY_EXISTS:
+            print(f"Mutex '{mutex_name}' already exists.")
+            return False
+        print(f"Mutex '{mutex_name}' created successfully.")
+        return True
+    def release_mutex(mutex_name):
+        kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+        mutex_handle = kernel32.OpenMutexW(0x1F0001, False, mutex_name)  # MUTEX_ALL_ACCESS
+        if mutex_handle is None:
+            raise ctypes.WinError(ctypes.get_last_error())
+        result = kernel32.ReleaseMutex(mutex_handle)
+        if not result:
+            raise ctypes.WinError(ctypes.get_last_error())
+        kernel32.CloseHandle(mutex_handle)
+        print(f"Mutex '{mutex_name}' released successfully.")
+
+def render():
+    pygame.init()
+    size = (200, 200)
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Multi Roblox')
+    image = pygame.image.load('mr.png')
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))
+        screen.blit(image, (0, 0))
+        pygame.display.flip()
+    pygame.quit()
+    sys.exit()
+
+def main():
+    Singleton.create_mutex("ROBLOX_singletonEvent") # create anti roblox mutex
+    render()
+
+if __name__ == "__main__":
+    main()
+    
